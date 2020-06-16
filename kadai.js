4e7b8f8c2e54967e9829036c90ccd7847a292fa7
taiga <gaitagaita69@gmail.com>
2020-06-16 14:06:58 +0900

    指摘事項の修正2

--- kadai.js
+++ kadai.js
@@ -7,14 +7,28 @@ window.onload = function () {
     // inputボタンの値を受け取る
     const addTask = (taskValue) => {
         const task = {
-            'ID':taskList.length,
-            'コメント':taskValue,
-            '状態':0
+            id: taskList.length,
+            comment: taskValue,
+            state: 0
         };
         taskList.push(task);
     }
+    // 「作業中」ボタンの追加
+    const addWorkBtn = () => {
+        const makeTd = document.createElement('td');
+        const makeBtn = document.createElement('button');
+        listArea.lastElementChild.appendChild(makeTd).appendChild(makeBtn);
+        listArea.lastElementChild.lastElementChild.lastElementChild.textContent = '作業中';
+    };
+    // 「削除」ボタンの追加
+    const addRemoveBtn = () => {
+        const makeTd = document.createElement('td');
+        const makeBtn = document.createElement('button');
+        listArea.lastElementChild.appendChild(makeTd).appendChild(makeBtn);
+        listArea.lastElementChild.lastElementChild.lastElementChild.textContent = '削除';
+    };
     // 追加ボタンが押下された時の処理
-    document.getElementById("addbtn").onclick = () => {
+    const addEvent = () => {
         // タスクリストのタイトル部分以外を削除
         while (listArea.childNodes[2]) {
             listArea.removeChild(listArea.childNodes[2] );
@@ -23,30 +37,21 @@ window.onload = function () {
         const taskValue = document.getElementById('task').value;
         // タスクの内容(taskValue)をタスクの追加関数(addTask)に渡す
         addTask(taskValue);
-        // ボタンの追加
-        const addBtn = (btnType) => {
-            const makeTd = document.createElement('td');
-            const makeBtn = document.createElement('button');
-            listArea.lastElementChild.appendChild(makeTd).appendChild(makeBtn);
-            listArea.lastElementChild.lastElementChild.lastElementChild.textContent = btnType;
-        };
-        // 「作業中」ボタンのテキスト
-        const textWork = '作業中';
-        // 「削除」ボタンのテキスト
-        const textDell = '削除';
         // フォームの内容をからにする
         document.getElementById('task').value = '';
         taskList.forEach(value => {
             const tr = document.createElement('tr');
             listArea.appendChild(tr);
             const tdId = document.createElement('td');
-            listArea.lastElementChild.appendChild(tdId).textContent = value['ID'];
+            listArea.lastElementChild.appendChild(tdId).textContent = value.id;
             const tdTask = document.createElement('td');
             listArea.lastElementChild.appendChild(tdTask);
-            listArea.lastElementChild.lastElementChild.textContent = value['コメント'];
-            addBtn(textWork);
-            addBtn(textDell);
+            listArea.lastElementChild.lastElementChild.textContent = value.comment;
+            addWorkBtn();
+            addRemoveBtn();
             }
         );
     };
+    // 追加ボタンが押下された時の処理
+    document.getElementById("addbtn").addEventListener('click',addEvent);
 }
